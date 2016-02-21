27ba8c89b675e3b65fcb6f1f50d040dea5fe661b
Vincent Bellittera <bellaproducts78@gmail.com>
2016-02-21 17:21:00 -0500

    Added validations and create actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,24 @@
 class ContactsController < ApplicationController
+	
 	def new
 		@contact = Contact.new
-	end
+	end # new, method or action
 
-		def create
-		end	
+	def create
+		@contact = Contact.new(contact_params)
+			if @contact.save
+				flash[:success] = "Message sent." 
+				redirect_to new_contact_path
+			else
+				flash[:danger] = "Error occurred, message has not been sent."
+				redirect_to new_contact_path
+			end # if,else
+	end # create method	or action
+
+	private
+		def contact_params
+			params.require(:contact).permit(:name, :email, :comments)
+		end # contact_params method or action
+			# the line of code above puts the parameters in a "white list"
+
 end
