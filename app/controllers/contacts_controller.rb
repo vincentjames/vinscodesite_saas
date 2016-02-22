2ccdc721e17e6c58568ffa0bf8ff2e29d6471806
Vincent Bellittera <bellaproducts78@gmail.com>
2016-02-21 19:01:59 -0500

    Added code to contacts_controller.rb file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,6 +12,7 @@ class ContactsController < ApplicationController
 				body = params[:contact][:comments]
 
 				ContactMailer.contact_email(name,email,body).deliver
+				
 				flash[:success] = "Message sent." 
 				redirect_to new_contact_path
 			else
